3e3fd7ba3b99bc871a4a032464ae492e1b105f3f
SLAB43 <SLAB43@KM102-09-M>
2026-02-26 10:58:15 +0100

    Architecture mvc

---
+++ F1_Live_Hub/Models/Utilisateur.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1_Live_Hub.Models
+{
+    internal class Utilisateur
+    {
+    }
+}
