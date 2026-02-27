75528586478df3d07b57c5be915f1cf5aff45f42
SLAB35 <SLAB35@KM101-04-M>
2026-02-27 09:40:41 +0100

    Debut metheo

--- F1_Live_Hub/Models/Meteo.cs
+++ F1_Live_Hub/Models/Meteo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,29 @@ namespace F1_Live_Hub.Models
 {
     internal class Meteo
     {
-    }
+     
+        
+
+
+public async Task<string> GetBlogdetails()
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync( "https://api.openf1.org/v1/weather");
+            if (response.IsSuccessStatusCode)
+            {
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return responseBody;
+            }
+            else
+            {
+                return "Error: " + response.StatusCode;
+            }
+
+
+        }
+    
+
 }
+}
+
+
