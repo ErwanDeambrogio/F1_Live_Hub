445cb70bd9db451706a195f7c554f7d5a6ed13f7
SLAB35 <SLAB35@KM102-08-M>
2026-03-05 11:57:14 +0100

    metheo 05/03/2026

--- F1_Live_Hub/Services/MeteoService.cs
+++ F1_Live_Hub/Services/MeteoService.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace F1_Live_Hub.Services
 {
