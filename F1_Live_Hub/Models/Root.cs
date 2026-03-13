b34bfe6f659210c9149f6f14de574bd8b3f9f6ed
SLAB43 <SLAB43@KM101-03-M>
2026-03-13 09:49:24 +0100

    13/03

---
+++ F1_Live_Hub/Models/Root.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1_Live_Hub.Models
+{
+    public class SessionRoot
+    {
+        public int session_key { get; set; }
+        public string session_type { get; set; }
+        public string session_name { get; set; }
+        public DateTime date_start { get; set; }
+        public DateTime date_end { get; set; }
+        public int meeting_key { get; set; }
+        public int circuit_key { get; set; }
+        public string circuit_short_name { get; set; }
+        public int country_key { get; set; }
+        public string country_code { get; set; }
+        public string country_name { get; set; }
+        public string location { get; set; }
+        public string gmt_offset { get; set; }
+        public int year { get; set; }
+    }
+}
